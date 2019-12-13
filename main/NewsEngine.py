2c1d7f12d5f5620799ec0222633caff8f56452ce
Shreyash7478 <shturkar90@gmail.com>
2019-12-13 19:13:32 +0530

    country and category unrecognised error solved

--- main/NewsEngine.py
+++ main/NewsEngine.py
@@ -1,7 +1,9 @@
 from newsapi import NewsApiClient
 
 news_subcategory = [ "business", "entertainment", "health", "science", "sports", "technology"]
-countries = {'argentina':'ar','australia':'au','austria':'at','aelgium':'be','arazil':'br','bulgaria':'bg','canada':'ca','china':'cn','colombia':'co','cuba':'cu','czech':'republic','cz':'egypt','eg':'france','fr':'germany','de':'greece','gr':'hong','kong':'hk','hungary':'hu','india':'in','indonesia':'id','ireland':'ie','israel':'il','italy':'it','japan':'jp','latvia':'lv','lithuania':'lt','malaysia':'my','mexico':'mx','morocco':'ma','netherlands':'nl','new':'zealand','nz':'nigeria','ng':'norway','no':'philippines','ph':'poland','pl':'portugal','pt':'romania','ro':'russia','ru':'saudi','arabia':'sa','serbia':'rs','singapore':'sg','slovakia':'sk','slovenia':'si','south':'africa','za':'south','korea':'kr','sweden':'se','switzerland':'ch','taiwan':'tw','thailand':'th','turkey':'tr','uae':'ae','ukraine':'ua','united':'kingdom','gb':'united','states':'us','venuzuela':'ve','argentina':'ar','australia':'au','austria':'at','belgium':'be','brazil':'br','bulgaria':'bg','canada':'ca','china':'cn','colombia':'co','cuba':'cu','czech':'republic','cz':'egypt','eg':'france','fr':'germany','de':'greece','gr':'hong','kong':'hk','hungary':'hu','india':'in','indonesia':'id','ireland':'ie','israel':'il','italy':'it','japan':'jp','latvia':'lv','lithuania':'lt','malaysia':'my','mexico':'mx','morocco':'ma','netherlands':'nl','new':'zealand','nz':'nigeria','ng':'norway','no':'philippines','ph':'poland','pl':'portugal','pt':'romania','ro':'russia','ru':'saudi','arabia':'sa','serbia':'rs','singapore':'sg','slovakia':'sk','slovenia':'si','south':'africa','za':'south','korea':'kr','sweden':'se','switzerland':'ch','taiwan':'tw','thailand':'th','turkey':'tr','uae':'ae','ukraine':'ua','united':'kingdom','gb':'united','states':'us','venuzuela':'ve'}
+countries = {'argentina':'ar','australia':'au','austria':'at','aelgium':'be','arazil':'br','bulgaria':'bg','canada':'ca','china':'cn','colombia':'co','cuba':'cu','czech republic':'cz','egypt':'eg','france':'fr','germany':'de','greece':'gr','hong kong':'hk','hungary':'hu','india':'in','indonesia':'id','ireland':'ie','israel':'il','italy':'it','japan':'jp','latvia':'lv','lithuania':'lt','malaysia':'my','mexico':'mx','morocco':'ma','netherlands':'nl','new zealand':'nz','nigeria':'ng','norway':'no','philippines':'ph','poland':'pl','portugal':'pt','romania':'ro','russia':'ru','saudi arabia':'sa','serbia':'rs','singapore':'sg','slovakia':'sk','slovenia':'si','south africa':'za','south korea':'kr','sweden':'se','switzerland':'ch','taiwan':'tw','thailand':'th','turkey':'tr','uae':'ae','ukraine':'ua','united kingdom':'gb','united states':'us','venuzuela':'ve'}
+
+
 newsapi = NewsApiClient(api_key='c9057c453e2446f5a4712ee31fd63ede')
 
 
@@ -20,6 +22,8 @@ def GetNews(search):
         else:
             words += i
     print("Result: ",category,country,words)
+
+    return
     if country and category:
         articles  = newsapi.get_top_headlines(q=words,country = country,category=category)['articles']
     elif country:
