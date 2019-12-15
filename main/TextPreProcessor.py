71df509f05144f6b26b985e8eaa7d9a339dafa99
Shreyash H. Turkar <33600638+Shreyash7478@users.noreply.github.com>
2019-12-15 13:32:02 +0530

    New TextProcessingSys

--- main/TextPreProcessor.py
+++ main/TextPreProcessor.py
@@ -1,35 +1,63 @@
-from textblob import TextBlob
 from nltk.corpus import stopwords 
-
-news_subcategory = [ "business", "entertainment", "health", "science", "sports", "technology"]
-countries = {'argentina':'ar','australia':'au','austria':'at','aelgium':'be','arazil':'br','bulgaria':'bg','canada':'ca','china':'cn','colombia':'co','cuba':'cu','czech republic':'cz','egypt':'eg','france':'fr','germany':'de','greece':'gr','hong kong':'hk','hungary':'hu','india':'in','indonesia':'id','ireland':'ie','israel':'il','italy':'it','japan':'jp','latvia':'lv','lithuania':'lt','malaysia':'my','mexico':'mx','morocco':'ma','netherlands':'nl','new zealand':'nz','nigeria':'ng','norway':'no','philippines':'ph','poland':'pl','portugal':'pt','romania':'ro','russia':'ru','saudi arabia':'sa','serbia':'rs','singapore':'sg','slovakia':'sk','slovenia':'si','south africa':'za','south korea':'kr','sweden':'se','switzerland':'ch','taiwan':'tw','thailand':'th','turkey':'tr','uae':'ae','ukraine':'ua','united kingdom':'gb','united states':'us','venuzuela':'ve'}
-countries_inverse = {'ar': 'argentina', 'au': 'australia', 'at': 'austria', 'be': 'aelgium', 'br': 'arazil', 'bg': 'bulgaria', 'ca': 'canada', 'cn': 'china', 'co': 'colombia', 'cu': 'cuba', 'cz': 'czech republic', 'eg': 'egypt', 'fr': 'france', 'de': 'germany', 'gr': 'greece', 'hk': 'hong kong', 'hu': 'hungary', 'in': 'india', 'id': 'indonesia', 'ie': 'ireland', 'il': 'israel', 'it': 'italy', 'jp': 'japan', 'lv': 'latvia', 'lt': 'lithuania', 'my': 'malaysia', 'mx': 'mexico', 'ma': 'morocco', 'nl': 'netherlands', 'nz': 'new zealand', 'ng': 'nigeria', 'no': 'norway', 'ph': 'philippines', 'pl': 'poland', 'pt': 'portugal', 'ro': 'romania', 'ru': 'russia', 'sa': 'saudi arabia', 'rs': 'serbia', 'sg': 'singapore', 'sk': 'slovakia', 'si': 'slovenia', 'za': 'south africa', 'kr': 'south korea', 'se': 'sweden', 'ch': 'switzerland', 'tw': 'taiwan', 'th': 'thailand', 'tr': 'turkey', 'ae': 'uae', 'ua': 'ukraine', 'gb': 'united kingdom', 'us': 'united states', 've': 'venuzuela'}
+from . import models
 
 stop_words = set(stopwords.words('english'))
 
-def preprocessing(query):
-    results = dict()
-    query = query.lower()
-    query_split = query.split()
-    new_query = []
-    year = []
-    for i in query_split:
+def wordFilter(word):
+    new_word = ""
+    for i in word:
+        if i.isalnum() or i.isspace(): 
+            new_word+=i
+    return new_word
+
+
+def GetQuotedText(search):
+    search = search.split("'")
+    quoted_search = []
+    simple_search = []
+    if len(search)%2:
+        for i in search[1:-1]:
+            if not i == "":
+                quoted_search.append(i)
+        if not search[0] == "":
+            simple_search.append(wordFilter(search[0]))
+        if not search[-1] == "":
+            simple_search.append(wordFilter(search[-1]))
+    else:
+        flag = False
+        for i in search:
+            if flag and not i == "":
+                quoted_search.append(i)
+            elif not i == "" :
+                simple_search.append(wordFilter(i))
+            flag = not flag
+    return (quoted_search,simple_search)
+
+def PreProcessor(simple_search):
+    words = simple_search.split(" ")
+    preprocesed = []
+    for i in words:
         if not i in stop_words:
-            temp = "".join([j for j in i if j.isalpha()])
-            if not temp == "": 
-                if temp in news_subcategory:
-                    results["category"] = temp
-                elif temp in countries:
-                    results["country_code"] = countries[temp]
-                    results["country"] = temp
-                elif temp in countries_inverse:
-                    results["country_code"] = temp
-                    results["country"] = countries_inverse[temp]
-                else:
-                    new_query.append(temp)
-            temp = "".join([j for j in i if j.isnumeric()])
-            if not temp == "" and int(temp)<2999: 
-                year.append(temp)
-    results["query"] = new_query
-    results["year"] = year
+            preprocesed.append(i)
+    return preprocesed
+
+def FilterPreProcesed(preprocesed):
+    language = ""
+    country = ""
+    category = ""
+    q = []
+    
+
+    return q,language,country,category
+
+def QueryProcessor(search):
+    results = dict()
+    q,temp = GetQuotedText(search)
+    preprocessed_words = PreProcessor(temp)
+    temp, language, country, category = FilterPreProcesed(preprocessed_words)
+    q.append(temp)
+    results["q"] = q
+    results["language"] = language
+    results["country"] = country
+    results["category"] = category
     return results
--- main/models.py
+++ main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Search(models.Model):
+    pk = models.AutoField(primary_key=True)
+    ip_location = models.CharField(max_length=50)
+    text_searched = models.CharField(max_length=100)
+    time = models.DateTimeField(auto_now=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=15)
+
+class Languages(models.Model):
+    name = models.CharField(max_length=75)
+
+
+class Countries(models.Model):
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=5)
+    capital_city = models.CharField(max_length=50)
+    language = models.ForeignKey(Languages,on_delete=models.CASCADE)
+
+class PropertiesOfSearch(models.Model):
+    q = models.CharField(max_length=50)
+    country = models.ForeignKey(Languages,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    language = models.ForeignKey(Languages,on_delete=models.CASCADE)
+
